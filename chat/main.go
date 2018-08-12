2c6345a777fa6336aab9ea7aeb7f228dd282ccaa
yorisilo <yorisilo@gmail.com>
2018-08-12 22:19:07 +0900

    add template

--- chat/main.go
+++ chat/main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's Chat!
-        </body>
-      </html>
-		`))
-	})
+// templ は1つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
+// ServeHTTP は HTTP リクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"}) // ServeHTTP method は templateHandlerのポインタ型(*templateHandler)に対しての method なので、 *templateHandler の値である &templateHandler として宣言しないと、ServeHTTP は使えない。
 	// Start Web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
